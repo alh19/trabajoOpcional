c267f9f80ff65a71bcd5f0c26031b004c3aeec68
MARCOS GONZÁLEZ RUIZ <Marcos.Gonzalez8@alu.uclm.es>
2022-10-01 14:19:31 +0200

    Configuración DbSet de las clases en ApplicationDbContext.cs

--- Sandwich2Go/src/Sandwich2Go/Data/ApplicationDbContext.cs
+++ Sandwich2Go/src/Sandwich2Go/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Sandwich2Go.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,9 @@ namespace Sandwich2Go.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Mesa> Mesa { get; set; }
+        public DbSet<MesaReserva> MesaReserva { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
