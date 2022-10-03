7dbe7f66e2380c82aacebb76b77d89bec95a8cd4
ALONSO ILLÁN MARTÍNEZ DEL SANTO <AlonsoIllan.Martinez@alu.uclm.es>
2022-10-03 16:45:13 +0200

    Modificados ApplicationDbContext

--- Sandwich2Go/Design/SandwichPedido.cs
+++ Sandwich2Go/Design/SandwichPedido.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using Sandwich2Go.Models;
 
 namespace Design
 {
