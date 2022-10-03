ff50cc5e5afb94b4bd23368492ae0a5046da3903
SANDRA MENDOZA MARTÍNEZ <Sandra.Mendoza2@alu.uclm.es>
2022-10-03 10:23:30 +0200

    Añadida propiedad Pedido a la clase ApplicationDbContext

--- Sandwich2Go/src/Sandwich2Go/Data/ApplicationDbContext.cs
+++ Sandwich2Go/src/Sandwich2Go/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Sandwich2Go.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +9,11 @@ namespace Sandwich2Go.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Pedido> Pedido { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
     }
 }
