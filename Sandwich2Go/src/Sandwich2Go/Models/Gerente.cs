8a2032736b4ae9e8136cc7f26504d09f264b485c
ALBERTO LÓPEZ HERNÁNDEZ <Alberto.Lopez31@alu.uclm.es>
2022-10-03 13:01:36 +0200

    Creadas clases Gerente, PedidoProv, ArticulosPed en Models y Design. Modificada clase Pedido en Models y Design. Añadidos campos ApplicacionDbContext

---
+++ Sandwich2Go/src/Sandwich2Go/Models/Gerente.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sandwich2Go.Models
+{
+    public class Gerente : IdentityUser
+    {
+            [Required]
+            public virtual string Nombre { get; set; }
+            [Required]
+            public virtual string Apellido { get; set; }
+            [Required]
+            public virtual string Direccion { get; set; }
+        }
+    }
+}
+}
