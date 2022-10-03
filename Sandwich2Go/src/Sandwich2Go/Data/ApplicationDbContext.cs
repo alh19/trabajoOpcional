5af11eed4b03d6a2a5c66f5aeeb73efb65ad2e3f
ALONSO ILLÁN MARTÍNEZ DEL SANTO <AlonsoIllan.Martinez@alu.uclm.es>
2022-10-03 12:06:29 +0200

    Corregido error en ApplicationDbContext

--- Sandwich2Go/src/Sandwich2Go/Data/ApplicationDbContext.cs
+++ Sandwich2Go/src/Sandwich2Go/Data/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Design;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Sandwich2Go.models;
