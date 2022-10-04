2bab6f836e20486d67a3c1024d7ac6eff580d871
MARCOS GONZÁLEZ RUIZ <Marcos.Gonzalez8@alu.uclm.es>
2022-10-04 17:33:05 +0200

    Creamos la Clase OfertaGerente, la configuramos y añadimos IList<OfertaGerente> a la clase Oferta

---
+++ Sandwich2Go/Design/OfertaGerente.cs
@@ -0,0 +1,20 @@
+﻿namespace Sandwich2Go.Models
+{
+    public class OfertaGerente
+    {
+        public Oferta Oferta
+        {
+            get => default;
+            set
+            {
+            }
+        }
+        public Gerente Gerente
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
