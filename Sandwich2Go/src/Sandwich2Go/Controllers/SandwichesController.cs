58e2204d4d97d3103a06efd00964c63fa72472ad
ALONSO ILLÁN MARTÍNEZ DEL SANTO <AlonsoIllan.Martinez@alu.uclm.es>
2022-11-04 10:43:03 +0100

    Comenzamos a implementar usuarios

--- Sandwich2Go/src/Sandwich2Go/Controllers/SandwichesController.cs
+++ Sandwich2Go/src/Sandwich2Go/Controllers/SandwichesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using Sandwich2Go.Models.SandwichViewModels;
 
 namespace Sandwich2Go.Controllers
 {
+    [Authorize]
     public class SandwichesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -54,8 +56,8 @@ namespace Sandwich2Go.Controllers
         {
             if (selectedSandwich.IdsToAdd != null)
             {
-
-                return RedirectToAction("Create", "Pedido", selectedSandwich);
+                Pedido pedido = new Pedido();
+                return RedirectToAction("Create", "Pedido", pedido);
             }
             //a message error will be shown to the customer in case no movies are selected
             ModelState.AddModelError(string.Empty, "Debes seleccionar al menos un Sándwich");
