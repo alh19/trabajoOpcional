632cda183619a8846e27fc2ea2a0dcf51e0f92c5
SANDRA MENDOZA MARTÍNEZ <Sandra.Mendoza2@alu.uclm.es>
2022-11-05 11:35:47 +0100

    Creada la clase SelectIngredientesForPurchaseViewModel

---
+++ Sandwich2Go/src/Sandwich2Go/Models/IngredienteViewModels/SelectIngredientesForPurchaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Sandwich2Go.Models.IngredienteViewModels
+{
+    public class SelectIngredientesForPurchaseViewModel
+    {
+
+        public IEnumerable<Ingrediente> Ingredientes { get; set; }
+        //Utilizado para filtrar por Alergeno
+        public SelectList Alergenos;
+        [Display(Name = "Alergeno")]
+        public string ingredienteAlergenoSelected { get; set; }
+        //Utilizado para filtrar por nombre del ingrediente
+        [Display(Name = "Nombre")]
+        public string ingredienteNombre
+        {
+            get; set;
+        }
+}
