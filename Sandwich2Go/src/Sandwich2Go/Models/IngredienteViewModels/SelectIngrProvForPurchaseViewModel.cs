884fb363087ee8702d1b448cb8c9183bf2608f79
ALBERTO LÓPEZ HERNÁNDEZ <Alberto.Lopez31@alu.uclm.es>
2022-11-08 21:24:18 +0100

    Clases Select del controlador IngrProv

---
+++ Sandwich2Go/src/Sandwich2Go/Models/IngredienteViewModels/SelectIngrProvForPurchaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Sandwich2Go.Models.ProveedorViewModels
+{
+    public class SelectIngrProvForPurchaseViewModel
+    {
+        public IEnumerable<Ingrediente> Ingredientes { get; set; }
+        //Utilizado para filtrar por nombres
+        [Display(Name = "Nombre")]
+        public string ingredienteNombre { get; set; }
+        [Display(Name = "Stock")]
+        public string ingredienteStock { get; set; }
+    }
+}
---
+++ Sandwich2Go/src/Sandwich2Go/Models/IngredienteViewModels/SelectedIngrProvForPurchaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Sandwich2Go.Models.ProveedorViewModels
+{
+    public class SelectedIngrProvForPurchaseViewModel
+    {
+
+        public string[] IdsToAdd { get; set; }
+        [Display(Name = "Nombre")]
+        public string ingredienteNombre { get; set; }
+        [Display(Name = "Stock")]
+        public string ingredienteStock { get; set; }
+
+    }
+}
