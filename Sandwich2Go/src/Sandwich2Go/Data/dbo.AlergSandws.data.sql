8e7b69939c692abc23b3cb4c8e9b44052c64a3c9
ALONSO ILLÁN MARTÍNEZ DEL SANTO <illan.martinez@hotmail.com>
2022-11-11 21:36:20 +0100

    Creada exportación de la BBDD e importación

--- Sandwich2Go/src/Sandwich2Go/Data/dbo.AlergSandws.data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO [dbo].[AlergSandws] ([AlergenoId], [IngredienteId], [Id]) VALUES (2, 1, 2)
-INSERT INTO [dbo].[AlergSandws] ([AlergenoId], [IngredienteId], [Id]) VALUES (3, 3, 3)
-INSERT INTO [dbo].[AlergSandws] ([AlergenoId], [IngredienteId], [Id]) VALUES (1, 4, 1)
