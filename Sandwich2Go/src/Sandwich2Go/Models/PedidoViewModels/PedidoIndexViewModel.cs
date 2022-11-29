1875aaf64347d4d352a75269d41c05a400f1400a
ALONSO ILLÁN MARTÍNEZ DEL SANTO <illan.martinez@hotmail.com>
2022-11-29 13:44:02 +0100

    Corregidos errores menores

--- Sandwich2Go/src/Sandwich2Go/Models/PedidoViewModels/PedidoIndexViewModel.cs
+++ Sandwich2Go/src/Sandwich2Go/Models/PedidoViewModels/PedidoIndexViewModel.cs
@@ -5,9 +5,12 @@ namespace Sandwich2Go.Models.PedidoViewModels
     public class PedidoIndexViewModel
     {
         public virtual int Id { get; set; }
+        [Display(Name = "Fecha de compra")]
         public virtual string fechaCompra { get; set; }
         [DataType(DataType.Currency)]
+        [Display(Name = "Precio")]
         public virtual double precio { get; set; }
+        [Display(Name = "Dirección de envío")]
         public virtual string direccion { get; set; }
 
         public PedidoIndexViewModel(Pedido p)
