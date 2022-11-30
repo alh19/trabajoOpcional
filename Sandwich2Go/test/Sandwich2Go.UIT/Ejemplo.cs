190efbb2f0d7efeca8c48bacb2280c6216c3f186
ALONSO ILLÁN MARTÍNEZ DEL SANTO <illan.martinez@hotmail.com>
2022-11-30 09:42:29 +0100

    Comenzadas a implementar las pruebas funcionales

---
+++ Sandwich2Go/test/Sandwich2Go.UIT/Ejemplo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Sandwich2Go.UIT
+{
+    public class Ejemplo : IDisposable
+    {
+        IWebDriver _driver;
+        string _URI = "https://localhost:5001/";
+
+        public Ejemplo()
+        {
+            var optionsc = new ChromeOptions
+            {
+                PageLoadStrategy = PageLoadStrategy.Normal,
+                AcceptInsecureCertificates = true
+            };
+
+            _driver = new ChromeDriver(optionsc);
+        }
+
+        [Fact]
+        public void Initial_step_opening_the_web_page2()
+        {
+            //Arrange
+            string expectedTitle = "Home Page - Sandwich2Go";
+            string expectedText = "Register";
+            //Act
+            //El navegador cargará la URI indicada
+            _driver.Navigate().GoToUrl(_URI);
+            //Assert
+            //Comprueba que el título coincide con el esperado
+            Assert.Equal(expectedTitle, _driver.Title);
+            //Comprueba si la página contiene el string indicado
+            Assert.Contains(expectedText, _driver.PageSource);
+        }
+
+
+        public void Dispose()
+        {
+            _driver.Close();
+            _driver.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
