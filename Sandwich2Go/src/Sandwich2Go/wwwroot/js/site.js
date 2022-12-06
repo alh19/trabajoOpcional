6a2431feea12e14f6e1fd796e9680b37767451e5
ALONSO ILLÁN MARTÍNEZ DEL SANTO <illan.martinez@hotmail.com>
2022-12-06 20:23:09 +0100

    Pruebas

--- Sandwich2Go/src/Sandwich2Go/wwwroot/js/site.js
+++ Sandwich2Go/src/Sandwich2Go/wwwroot/js/site.js
@@ -9,3 +9,26 @@ $('#r11').on('click', function () {
 $('#r12').on('click', function () {
     $(this).parent().find('a').trigger('click')
 })
+
+function updatingPrices() {
+
+    //  let element = document.getElementById("TotalAccount");
+    let elements = document.getElementsByName("Cantidades");
+
+    let total = 0;
+    for (let i = 0; i < elements.length; i++) {
+        total = total + parseInt(elements[i].value);
+    }
+    console.log(total);
+    document.getElementById("CantidadTotal").innerHTML = 3;
+
+}
+
+const formatter = new Intl.NumberFormat('es-ES', {
+    style: 'currency',
+    currency: 'EUR',
+
+    // These options are needed to round to whole numbers if that's what you want.
+    minimumFractionDigits: 2, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    maximumFractionDigits: 2, // (causes 2500.99 to be printed as $2,501)
+});
