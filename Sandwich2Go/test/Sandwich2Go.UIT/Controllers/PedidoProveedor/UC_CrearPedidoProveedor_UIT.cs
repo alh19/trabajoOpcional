271a4fb80cea33837ddc2f3cf41eb72891730feb
SANDRA MENDOZA MARTÍNEZ <Sandra.Mendoza2@alu.uclm.es>
2022-12-15 14:27:58 +0100

    Acabadas pruebas funcionales UC_CrearPedidoProveedor

--- Sandwich2Go/test/Sandwich2Go.UIT/Controllers/PedidoProveedor/UC_CrearPedidoProveedor_UIT.cs
+++ Sandwich2Go/test/Sandwich2Go.UIT/Controllers/PedidoProveedor/UC_CrearPedidoProveedor_UIT.cs
@@ -6,11 +6,18 @@ using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 //Necesario para obtener Find dentro de las ICollection o IList
 using System.Linq;
 using System.Text;
 using System.Threading;
 using Xunit;
+using Sandwich2Go;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace Sandwich2Go.UIT.Controllers.PedidoProveedor
 {
@@ -39,6 +46,121 @@ namespace Sandwich2Go.UIT.Controllers.PedidoProveedor
 
             _driver = new ChromeDriver(optionsc);
         }
+
+        /*
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            var role = serviceProvider.GetRequiredService(typeof(RoleManager<IdentityRole>));
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+
+            List<string> rolesNames = new List<string> { "Gerente", "Cliente" };
+
+            SeedRoles(roleManager, rolesNames);
+            SeedUsers(userManager, rolesNames);
+
+            DirectoryInfo directoryData = new DirectoryInfo(".\\Data");
+
+            // Se obtienen todos los ficheros localizados en .\AppForMovies\Data que contienen un ".sql" en su nombre
+            foreach (FileInfo item in directoryData.GetFiles().Where(m => m.Name.Contains("dbo.SQLProv.dataF.sql")))
+            {
+                try
+                {
+                    // Se lee el contenido del fichero ".sql"
+                    string commandSQL = item.OpenText().ReadToEnd();
+
+
+                    // Se ejecuta el contenido del fichero ".sql"
+                    dbContext.Database.ExecuteSqlRaw(commandSQL);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+
+
+        }
+
+
+
+
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager, List<string> roles)
+        {
+
+            foreach (string roleName in roles)
+            {
+                //it checks such role does not exist in the database 
+                if (!roleManager.RoleExistsAsync(roleName).Result)
+                {
+                    IdentityRole role = new IdentityRole();
+                    role.Name = roleName;
+                    role.NormalizedName = roleName;
+                    IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+                }
+            }
+
+        }
+
+        public static void SeedUsers(UserManager<IdentityUser> userManager, List<string> roles)
+        {
+            //first, it checks the user does not already exist in the DB
+            if (userManager.FindByNameAsync("elena@uclm.com").Result == null)
+            {
+                Gerente user = new Gerente();
+                user.Id = "1";
+                user.UserName = "elena@uclm.com";
+                user.Email = "elena@uclm.com";
+                user.Nombre = "Elena";
+                user.Apellido = "Navarro Martinez";
+                user.EmailConfirmed = true;
+                user.Direccion = "";
+
+                var result = userManager.CreateAsync(user, "Password1234%");
+                result.Wait();
+
+                if (result.IsCompletedSuccessfully)
+                {
+                    //administrator role
+                    userManager.AddToRoleAsync(user, roles[0]).Wait();
+                }
+            }
+
+            if (userManager.FindByNameAsync("gregorio@uclm.com").Result == null)
+            {
+                Cliente user = new Cliente();
+                user.Id = "2";
+                user.UserName = "gregorio@uclm.com";
+                user.Email = "gregorio@uclm.com";
+                user.Nombre = "Gregorio";
+                user.Apellido = "Diaz Descalzo";
+                user.EmailConfirmed = true;
+                user.Direccion = "";
+
+                var result = userManager.CreateAsync(user, "APassword1234%");
+                result.Wait();
+
+                if (result.IsCompletedSuccessfully)
+                {
+                    //employee role
+                    userManager.AddToRoleAsync(user, roles[1]).Wait();
+                }
+            }
+
+        }
+
+        */
+
+
+
+
+
+
+
         private void Precondition_perform_login(string username, string password)
         {
             _driver.Navigate().GoToUrl(_URI +
